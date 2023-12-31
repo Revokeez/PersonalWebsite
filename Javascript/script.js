dd99fe2a5470f1b8e4947496204620c93a9eadeb
Edwin <revokeezjn@gmail.com>
2023-12-31 13:17:47 -0400

    Resising Image

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,16 @@
-function changeColor() {
-
-    document.body.style.backgroundColor = "blue";
+function resizing()
+{
+    let width = window.outerWidth;
+    let height = window.outerHeight;
+    let response = "WIndow width: " + width + ", window height: " + height; 
+    console.log(response);
+    if(width < 420 && height < 870)
+    {
+        document.getElementById("img").style.visibility = "hidden";
+    }
+    else
+    {
+        document.getElementById("img").style.visibility = "visible";
+    }
 
 }
