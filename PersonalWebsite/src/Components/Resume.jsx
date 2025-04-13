991fdc021602e8808d6da179cc4961d074ea06a8
Revok <revokeezjn@gmail.com>
2025-04-13 09:27:45 -0400

    Added margin top and botton im Resumen.jsx component

--- PersonalWebsite/src/Components/Resume.jsx
+++ PersonalWebsite/src/Components/Resume.jsx
@@ -4,7 +4,7 @@ import resume from "../assets/Resume/Edwin Manuel Vazquez Vega resume 2025.pdf"
 
 const Resume = () => {
     return (
-        <div className="flex flex-row justify-center items-center" id="dowloadresume">
+        <div className="flex flex-row justify-center items-center my-4" id="dowloadresume">
             <a href={resume} download={true} className="btn btn-gradient btn-primary waves waves-light">Dowload Resume</a>
         </div>
 
