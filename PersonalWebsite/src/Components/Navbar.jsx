8a57c177a96d93a2eace4d8244aea743b3625411
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-04-19 01:13:58 -0300

    Remove Dropdown from navbar

--- PersonalWebsite/src/Components/Navbar.jsx
+++ PersonalWebsite/src/Components/Navbar.jsx
@@ -12,24 +12,12 @@ const Navbar = () => {
                             href="#">
                             Edwin
                         </a>
-                        <div className="md:hidden">
-                            <button
-                                type="button"
-                                className="collapse-toggle btn btn-outline btn-secondary btn-sm btn-square"
-                                data-collapse="#default-navbar-collapse"
-                                aria-controls="default-navbar-collapse"
-                                aria-label="Toggle navigation"
-                            >
-                                <span className="icon-[tabler--menu-2] collapse-open:hidden size-4"></span>
-                                <span className="icon-[tabler--x] collapse-open:block hidden size-4"></span>
-                            </button>
-                        </div>
+
                     </div>
                 </div>
                 <div
-                    id="default-navbar-collapse"
-                    className="md:navbar-end collapse hidden grow basis-full overflow-hidden transition-[height] duration-300 max-md:w-full">
-                    <ul className="menu md:menu-horizontal gap-2 p-0 text-base max-md:mt-2">
+                    className="md:navbar-end grow max-sm:hidden">
+                    <ul className="menu menu-horizontal gap-2 p-0 text-base max-md:mt-2">
                         <li><a href="#whoami">Who am I</a></li>
                         <li><a href="#projectsandawards">Projects and Awards</a></li>
                         <li><a href="#workexperience">Work Experience</a></li>
