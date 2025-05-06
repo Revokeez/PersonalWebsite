7a73e37ae7cdb23a8596194b67ebc9c50adf257f
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-05-06 17:04:02 -0300

    Updated WhoAmI Component. added md:text-xl for responsiveness

--- PersonalWebsite/src/Components/WhoAmI.jsx
+++ PersonalWebsite/src/Components/WhoAmI.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 
 //Components
-import React, { useEffect }  from "react";
+import React from "react";
 
 
 const WhoAmI = () => {
   return (
     <div className="font-mono text-center mt-10">
-      <h1 className=" text-3xl">Who am I?</h1>
-      <div>
+      <h1 className="text-3xl">Who am I?</h1>
+      <div className="md:text-xl">
         <p>👩‍💻💡Enthusiastic computer science student on a quest to turn my passion for coding into real world solutions!</p> 
         <p>🚀 Seeking internship opportunities to dive into the world of code, solve puzzles, and make a real impact.</p>
       </div>
