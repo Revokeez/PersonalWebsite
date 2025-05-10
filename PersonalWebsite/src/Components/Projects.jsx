6c6dbad437c032890924b409711c90003edbbf3f
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-05-10 15:45:18 -0300

    Added new assoation website card in Project component

--- PersonalWebsite/src/Components/Projects.jsx
+++ PersonalWebsite/src/Components/Projects.jsx
@@ -1,29 +1,27 @@
-
-
 /* eslint-disable no-unused-vars */
+import React from "react";
 
 //Components
-import React from "react";
+
 import Card from "./Card";
 
 //Images
-import Assosiation_Website_IMG from "../assets/infotech_homePage_img.png"
+import Assosiation_Website_IMG from "../assets/Infotech_Website_New.png"
+import Assosiation_Website_IMG_V1 from "../assets/infotech_homePage_img.png"
 import Catch_Phishing from "../assets/catchphishing.png"
 import Img_Not_Found from "../assets/not_found_img.png"
 
 const Awards = () =>{
-    const associationWebsite = `Designed and developed a responsive website using HTML, Tailwind CSS, and JavaScript to enhance the 
-    online presence of the student association. Implemented a streamlined Git and GitHub Actions pipeline to enable continuous integration
-    and automated deployments.`;   
+    const associationWebsite = `Successfully developed the latest version of the Association website using modern technologies like React
+    and Tailwind CSS. Additionally, Implemented a streamlined Git and GitHub Actions pipeline to enable continuous integration
+    and automated deployments..`
+
+    const associationWebsiteV1 = `Designed and developed a responsive website using HTML, Tailwind CSS, and JavaScript to enhance the 
+    online presence of the student association.`;   
     
     const catchPhisingWebsite = `Catch Phish is an online platform designed to teach users how to spot and avoid phishing emails.
     Developed under the "Phish Training" submodel for the Master Workforce Research Internship, it aims to help people protect
     themselves from online threats.`
-
-    const pidss = `The Phishing Identification and Decision Support System (PIDSS) is a tool developed as part of the 
-    Master Workforce Internship 2023. This tool was made to help with identifying Phishing emails by
-    properly scanning for commonly known strategies. This tool has an easy to understand Interface where
-    users can see the ”Phishing Percentage” based on how likely the email provided is to be a Phishing attack.`
 
     return (
         <>
@@ -41,18 +39,15 @@ const Awards = () =>{
             <div className="text-center font-mono mt-10" data-aos="zoom-in">
                 <h1 className="text-3xl">Projects</h1>
                     <div className="flex justify-center flex-wrap">
+                        <Card img={Assosiation_Website_IMG}
+                            title="Assosiation Website"
+                            text={associationWebsite}
+                            showLinkOfWebsite="https://infotech-uiprb.com/index.html" 
+                            showGithubLink="https://github.com/InfoTech-UIPRB/Info-tech" /> 
 
-                        <Card img={Assosiation_Website_IMG} showGithubLink="https://github.com/InfoTech-UIPRB/Info-tech" 
-                              showLinkOfProject="https://infotech-uiprb.com/index.html" 
-                              title="Assosiation Website" 
-                              text={associationWebsite}/>  
-
-                        {/* 
-                              <Card img={Img_Not_Found}
-                              title="Phishing Identification and Decision Support System (PIDSS) Master Workfoce Research Internship"
-                              text={pidss}
-                              showGithubLink="https://github.com/nordiel/PIDSS"/> 
-                        */}
+                        <Card img={Assosiation_Website_IMG_V1} showGithubLink="https://github.com/InfoTech-UIPRB/Info-tech" 
+                              title="Assosiation Website Version 1" 
+                              text={associationWebsiteV1}/>  
 
                         <Card img={Catch_Phishing} 
                               title="CatchPhising Website Master Workfoce Research Internship" 
